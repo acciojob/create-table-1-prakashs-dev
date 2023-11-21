e68502f2d799b4c23dda373ddc03a6616e1eb212
Prakash S <79446554+prakashs-dev@users.noreply.github.com>
2023-11-21 12:20:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
 function insert_Row() {
     //Write your code here
-  
+  const rowCreate = document.createElement("tr");
+  for (let i = 1; i <= 2; i++) {
+    const cellCreate = document.createElement("td");
+    cellCreate.innerText = `New Cell${i}`;
+    rowCreate.appendChild(cellCreate);
+  }
+  const parent = document.getElementById("sampleTable");
+  const nxtSibling = document.querySelector("#sampleTable > tbody");
+  parent.insertBefore(rowCreate, nxtSibling);
   
 }
